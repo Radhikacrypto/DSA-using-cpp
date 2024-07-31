fa634eca11c384db950bcd4d5d7bcc98d749597f
Radhika Bacharwar <radhikabacharwar20@gmail.com>
2024-07-31 16:03:03 +0530

    leetcode ques 1009

---
+++ 7_1009.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<
+using namespace std;
+class solution{
+    
+
+};
+int main(){
+    solution s;
+    int n;
+    cout<<"enter a number"<<endl;
+    cin>>n;
+    int complement= s.BitwiseComplement(n);
+    cout<<complement;
+    return 0;
+}
---
+++ rough.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=101;
+    int b=a^111;
+    cout<<b;
+
+    return 0;
+}
