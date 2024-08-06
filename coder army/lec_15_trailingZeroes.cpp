1300f509e0c241739d0fb44d67a3ff054cbfab6d
Radhika Bacharwar <radhikabacharwar20@gmail.com>
2024-08-07 01:06:47 +0530

    lect-15

--- coder army/lec_15_trailingZeroes.cpp
+++ coder army/lec_15_trailingZeroes.cpp
@@ -1,10 +1,34 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
-class solution{
-    int factorial(int n){
-        int ans
-    }
-}
-int main(){
+
+
+class solution {
+public:
+// int trailingZeroes(int n){
+//     int zeroes=0;
+//     while(n>5){
+//         zeroes+=n/5;
+//         n=n/5;
+//     }
+//     return zeroes;
+// }
+int zeroes = 0;
+        int i = 5;
+        while (n / i >= 1) {
+            zeroes += n / i;
+            i *= 5;
+        }
+    
+};
+
+int main() {
+    int n;
+    cout << "Enter a number " << endl;
+    cin >> n;
+    solution s;
+    int result = s.trailingZeroes(n);
+    cout << "Number of trailing zeroes is " << result << endl;
+
     return 0;
 }
